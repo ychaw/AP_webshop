ea0ab15e0dbb64568ac45648ad64459a402d45c7
ychaw <44437905+ychaw@users.noreply.github.com>
2018-10-24 18:33:04 +0200

    project setup
    failed to install sqlite3 until now

---
+++ server.js
@@ -0,0 +1,36 @@
+// Datenbank initialisieren
+const sqlite3 = require('sqlite3').verbose();
+let userDB = new sqlite3.Database('user.db');
+let productDB = new sqlite3.Database('product.db');
+
+// Express.js Webserver
+const express = require('express');
+const app = express()
+
+// Body-Parser: wertet POST-Formulare aus
+const bodyParser= require('body-parser')
+app.use(bodyParser.urlencoded({extended: true}))
+
+// EJS Template Engine
+app.engine('.ejs', require('ejs').__express);
+app.set('view engine', 'ejs');
+
+// Sessionvariablen
+const session = require('express-session');
+app.use(session({
+	secret: 'example',
+	resave: false,
+	saveUninitialized: true
+}));
+
+// Passwort Verschlüsselung
+const passwordHash = require('password-hash');
+
+// Webserver starten http://localhost:3000
+app.listen(3000, function(){
+	console.log("listening on 3000");
+});
+
+app.get("/", function(req, res) {
+  res.render("index");
+});
