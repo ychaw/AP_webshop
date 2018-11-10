8a89224cbec8922cb67fe2c11aec6fd3a7e30295
ychaw <44437905+ychaw@users.noreply.github.com>
2018-11-10 16:48:28 +0100

    Changed reg. Implemented hash. Fixed crash on deleting account.

--- server.js
+++ server.js
@@ -8,8 +8,10 @@ const express = require('express');
 const app = express()
 
 // Body-Parser: wertet POST-Formulare aus
-const bodyParser= require('body-parser')
-app.use(bodyParser.urlencoded({extended: true}))
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
 // EJS Template Engine
 app.engine('.ejs', require('ejs').__express);
@@ -18,22 +20,23 @@ app.set('view engine', 'ejs');
 // Sessionvariablen
 const session = require('express-session');
 app.use(session({
-	secret: 'example',
-	resave: false,
-	saveUninitialized: true,
-	sessionUser: false
+  secret: 'example',
+  resave: false,
+  saveUninitialized: true,
+  sessionUser: false
 }));
 
 // Passwort Verschlüsselung
-const passwordHash = require('bcrypt');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 //Include CSS and JS directories
 app.use(express.static(__dirname + "/stylings"));
 app.use(express.static(__dirname + "/js"));
 
 // Webserver starten http://localhost:3000
-app.listen(3000, function(){
-	console.log("listening on 3000");
+app.listen(3000, function() {
+  console.log("listening on 3000");
 });
 
 // Websites
@@ -41,8 +44,8 @@ app.get("/", function(req, res) {
   res.redirect("home");
 });
 
- //DB erstellen, später auskommentieren
- /*
+//DB erstellen, später auskommentieren
+/*
 app.get("/", (request,response) =>{
 	userDB.run('CREATE TABLE user (id_user INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL, password TEXT NOT NULL)');
 	productDB.run('CREATE TABLE user (id_product INTEGER PRIMARY KEY AUTOINCREMENT, productname TEXT NOT NULL, price TEXT NOT NULL, quantity TEXT NOT NULL)');
@@ -70,112 +73,155 @@ app.get("/newproduct", function(req, res) {
 });
 
 app.get("/delete", function(req, res) {
-	res.render("delete");
+  res.render("delete");
 });
 
 app.get("/logout", function(req, res) {
-	sessionUser=false;
-	res.render("logout");
+  sessionUser = false;
+  res.render("logout");
 });
 
 
 /*Register user*/
 
 app.post('/registration', (req, res) => {
-	const newUser = req.body["user"];
-	const newPswd1 = req.body["pw1"];
-	const newPswd2 = req.body["pw2"];
+  const newUser = req.body["name"];
+  const newPswd1 = req.body["pw1"];
+  const newPswd2 = req.body["pw2"];
+  var hashedPswd = false;
+  var nameTaken = false;
 
-	if (newUser in users || newPswd1 != newPswd2 || newUser == '' || newPswd1 == '' || newPswd2 == '') {
-		// error
-		// Hab erstmal das hier eingefügt...
-		// Kannst du natürlich noch schöner machen
-		// Aber so ist erstmal eine Funktionalität vorhanden
-		if (newUser in users) {
-			res.write("<html><body><li>Username is already taken.</li></body></html>");
-		}
-		if (newPswd1 != newPswd2) {
-			res.write("<html><body><li>Passwords are not the same.</li></body></html>");
-		}
-		if (newUser == '') {
-			res.write("<html><body><li>Username is empty.</li></body></html>");
-		}
-		if (newPswd1 == '') {
-			res.write("<html><body><li>First Password is empty.</li></body></html>");
-		}
-		if (newPswd2 == '') {
-			res.write("<html><body><li>Second Password is empty.</li></body></html>");
-		}
-		res.write(`
-			<br>
-			<form action="/registration" method="GET">
-					<button type="submit">Try again</button>
-			</form>
-		`);
-		res.end();
-	} else {
-		db.run(`INSERT INTO users (userName, userPW) VALUES ('${newUser}', '${newPswd}')`, (error) => {
-			if (error) {
-				console.log(error.message);
-				res.render("error", {"msg" : error.message});
-			}
-		});
-		//show successfull registration prompt or something similar. Then redirect to login
-		res.redirect('/login');
-	}
+  userDB.get(`SELECT * FROM user WHERE username='${newUser}'`, (error, row) => {
+    if (row != undefined) {
+      nameTaken = true;
+    }
+    if (error) {
+      console.log(error.message);
+    }
+    if (nameTaken || newPswd1 != newPswd2 || newUser == '' || newPswd1 == '' || newPswd2 == '') {
+      // error
+      // Hab erstmal das hier eingefügt...
+      // Kannst du natürlich noch schöner machen
+      // Aber so ist erstmal eine Funktionalität vorhanden
+      if (nameTaken) {
+        res.render("error", {
+          "msg": "Username is already taken."
+        });
+      }
+      if (newPswd1 != newPswd2) {
+        res.render("error", {
+          "msg": "Passwords are not the same."
+        });
+      }
+      if (newUser == '') {
+        res.render("error", {
+          "msg": "Username is empty."
+        });
+      }
+      if (newPswd1 == '') {
+        res.render("error", {
+          "msg": "First Password is empty."
+        });
+      }
+      if (newPswd2 == '') {
+        res.render("error", {
+          "msg": "Second Password is empty."
+        });
+      }
+    } else {
+      bcrypt.hash(newPswd1, saltRounds, function(error, hash) {
+        console.log(hashedPswd);
+        if (error) {
+          console.log(error.message);
+          res.render("error", {
+            "msg": error.message
+          });
+        }
+
+        hashedPswd = hash;
+        console.log(hashedPswd);
+        userDB.run(`INSERT INTO user (username, password) VALUES ('${newUser}', '${hashedPswd}')`, (error) => {
+          if (error) {
+            console.log(error.message);
+            res.render("error", {
+              "msg": error.message
+            });
+          }
+        });
+
+        //show successfull registration prompt or something similar. Then redirect to login
+        res.redirect('/login');
+      });
+    }
+  });
+
 });
 
 /*Delete user*/
 
 app.post('/delete', (req, res) => {
-	const user = sessionUser;
-	const password = req.body["pw"];
+  const user = sessionUser;
+  const password = req.body["pw"];
 
-	userDB.get(`SELECT * FROM user WHERE username='${sessionUser}'`,(error,row)=>{
-			if (row != undefined){
-					if (password == row.password){
-						sessionUser=false;
+  userDB.get(`SELECT * FROM user WHERE username='${sessionUser}'`, (error, row) => {
+    if (row != undefined) {
+      const hash = row.password;
+      bcrypt.compare(password, hash, function(error, isCorrect) {
+        if (isCorrect) {
+          sessionUser = false;
 
-					} else {
-							res.render('error', {"msg" : "Wrong password"});
-					}
-			} else {
-					res.render('error'), {"msg" : "Row is undefined"};
-			}
-			if(!sessionUser) {
-				console.log("Delete now", user, password);
-				userDB.run(`DELETE FROM user WHERE username='${user}'`, (error) => {
-					console.log("Callback from delete request");
-						if (error){
-								console.log(error.message);
-						}
-						res.redirect('/home');
-				});
-			} else {
-				res.render("error", {"msg" : "Logging out failed"});
-			}
+          console.log("Delete now", user, password);
 
-			console.log(sessionUser);
-	});
-
+          userDB.run(`DELETE FROM user WHERE username='${user}'`, (error) => {
+            console.log("Callback from delete request");
+            if (error) {
+              console.log(error.message);
+            }
+            res.redirect('/home');
+          });
+        } else {
+          res.render('error', {
+            "msg": "Wrong password"
+          });
+        }
+      });
+    } else {
+      res.render('error'), {
+        "msg": "Row is undefined"
+      };
+    }
+    console.log(sessionUser);
+  });
 });
 
 /*Login*/
 
-app.post('/login', function (req, res) {
-	const user = req.body["name"];
-	const password = req.body["pw"];
+app.post('/login', function(req, res) {
+  const user = req.body["name"];
+  const password = req.body["pw"];
 
-	userDB.get(`SELECT * FROM user WHERE username='${user}'`,(error,row)=>{
-			if (row != undefined){
-					if (password == row.password){
-							sessionUser = user;
-							res.render('success', { 'user': user });
-					} else {
-							res.render('error', {"msg" : "Wrong password"});
-					}
-			} else {
-					res.render('error', {"msg" : "No password found"});
-			}
-	});
+  userDB.get(`SELECT * FROM user WHERE username='${user}'`, (error, row) => {
+
+    if (row != undefined) {
+      const hash = row.password;
+      bcrypt.compare(password, hash, function(error, isCorrect) {
+
+        if (isCorrect) {
+          sessionUser = user;
+          res.render('success', {
+            'user': user
+          });
+        } else {
+          res.render('error', {
+            "msg": "Wrong password"
+          });
+        }
+      });
+
+    } else {
+      res.render('error', {
+        "msg": "Name or password not found"
+      });
+    }
+  });
 });
